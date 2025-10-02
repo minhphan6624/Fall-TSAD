a32d6e9843e1e01fbd1e8c1137fdeb01f19bed84
minhphan6624 <quangminh6624@gmail.com>
2025-10-02 17:51:48 +0930

    modify pipeline run script to use proper segmentation params

--- src/pipelines/sisfall/aa_run_pipeline.py
+++ src/pipelines/sisfall/aa_run_pipeline.py
@@ -22,8 +22,20 @@ def run_pipeline(cfg):
     # 3–4. Normalize
     normed = normalize_splits(splits)
 
+    # Calculate window size and overlap in time steps ()
+    # Example: windows size: 3seconds * 200Hz = 600 time steps
+    # Overlap: 50% of 600 = 300 time steps
+
+    window_size = int(cfg.data.segment_seconds * cfg.data.sampling_rate)
+    if cfg.data.overlap >= window_size:
+        raise ValueError("Overlap must be less than window size.")
+    
+    overlap = cfg.data.overlap * window_size if cfg.data.overlap < 1 else cfg.data.overlap
+
+    print(f"Window size (in time steps): {window_size}")
+    print(f"Overlap (in time steps): {overlap}")
+
+    overlap = cfg.data.overlap
     # 5–6. Segment + serialize
-    serialize(normed, out_dir, 
-              window=int(cfg.data.segment_seconds*cfg.data.sampling_rate),
-              overlap=cfg.data.overlap)
+    serialize(normed, out_dir, window=window_size,overlap=overlap)
 
