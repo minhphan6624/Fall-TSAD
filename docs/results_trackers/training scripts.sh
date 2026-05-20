3512608b36802ede13d8c495f0e93f835b32cc7f
Minh Phan <quangminh6624@gmail.com>
2026-05-20 21:04:59 +0930

    runscripts

--- docs/results_trackers/training scripts.sh
+++ docs/results_trackers/training scripts.sh
@@ -50,3 +50,49 @@ python -m src.training.aggregate_cv_metrics --dataset-prefix "$prefix" --n-folds
 python -m src.training.aggregate_cv_metrics --dataset-prefix "$prefix" --n-folds 5 --mode tsad --model lstm_ae --model-seed 42
 
 python -m src.training.aggregate_cv_metrics --dataset-prefix "$prefix" --n-folds 5 --mode tsad --model cnn1d_ae --model-seed 42
+
+for prefix in \
+  sisfall_easy_adl_20hz_2s \
+  fallalld_easy_adl_20hz_2s \
+  umafall_easy_adl_20hz_2s \
+  upfall_easy_adl_20hz_2s
+do
+  for fold in 0 1 2 3 4; do
+    dataset="${prefix}_fold${fold}"
+    
+  python -m src.training.train_dense_ae --dataset "$dataset" --model-seed 42 --device auto
+  
+  done
+done
+
+    python -m src.training.train_random_forest --dataset "$dataset" --model-seed 42
+    python -m src.training.train_xgboost --dataset "$dataset" --model-seed 42
+    python -m src.training.train_isolation_forest --dataset "$dataset" --model-seed 42
+
+    python -m src.training.train_cnn1d --dataset "$dataset" --model-seed 42 --device auto
+    python -m src.training.train_cnn1d_ae --dataset "$dataset" --model-seed 42 --device auto
+
+    python -m src.training.train_lstm_ae --dataset "$dataset" --model-seed 42 --device auto
+    python -m src.training.train_lstm_classifier --dataset "$dataset" --model-seed 42 --device auto
+    python -m src.training.train_dense_ae --dataset "$dataset" --model-seed 42 --device auto
+
+for prefix in \
+  sisfall_easy_adl_20hz_2s \
+  fallalld_easy_adl_20hz_2s \
+  umafall_easy_adl_20hz_2s \
+  upfall_easy_adl_20hz_2s
+do
+  python -m src.training.aggregate_cv_metrics --dataset-prefix "$prefix" --n-folds 5 --mode classification --model random_forest --model-seed 42
+  python -m src.training.aggregate_cv_metrics --dataset-prefix "$prefix" --n-folds 5 --mode classification --model xgboost --model-seed 42
+
+
+  python -m src.training.aggregate_cv_metrics --dataset-prefix "$prefix" --n-folds 5 --mode tsad --model isolation_forest --model-seed 42
+  
+done
+
+  python -m src.training.aggregate_cv_metrics --dataset-prefix "$prefix" --n-folds 5 --mode classification --model cnn1d --model-seed 42
+  python -m src.training.aggregate_cv_metrics --dataset-prefix "$prefix" --n-folds 5 --mode classification --model lstm_classifier --model-seed 42
+
+  python -m src.training.aggregate_cv_metrics --dataset-prefix "$prefix" --n-folds 5 --mode tsad --model dense_ae --model-seed 42
+  python -m src.training.aggregate_cv_metrics --dataset-prefix "$prefix" --n-folds 5 --mode tsad --model cnn1d_ae --model-seed 42
+  python -m src.training.aggregate_cv_metrics --dataset-prefix "$prefix" --n-folds 5 --mode tsad --model lstm_ae --model-seed 42
